24ba66ba05b7920312385eff7081934e3425c250
ghattamaneni.uday@gmail.com <ghattamaneni.uday@gmail.com@3013a926-2a29-a017-9e15-02485f5b1ae8>
2011-02-26 06:49:29 +0000

    Added Repository structure and jar's needed . Also added Inventory entry and Inventories Repository class files

---
+++ trunk/InventoryControl/src/org/workplicity/inventorycontrol/repos/Inventories.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.workplicity.inventorycontrol.repos;
+
+import org.workplicity.inventorycontrol.entry.Inventory;
+import org.workplicity.repos.Repository;
+
+/**
+ *
+ * @author Uday
+ */
+public class Inventories extends Repository<Inventory> {
+    public final static String TITLE = "Inventories";
+
+    /**
+     * Constructor
+     */
+    public Inventories() {
+        super(TITLE);
+    }
+
+
+}
