f03561d85a3f0970f6ee5b8f565e386a14216cbe
BGormanly@gmail.com <BGormanly@gmail.com@3013a926-2a29-a017-9e15-02485f5b1ae8>
2011-04-23 19:54:41 +0000

    New testing working... not yet ready...

--- trunk/InventoryControl/server/test/org/workplicity/inventorycontrol/test/ItemCreateTest.java
+++ trunk/InventoryControl/server/test/org/workplicity/inventorycontrol/test/ItemCreateTest.java
@@ -27,27 +27,23 @@ public class ItemCreateTest extends Test {
         try {
             
             ItemCreateTest thisTest = new ItemCreateTest();
-                
-            String criteria1 = "/list";
-            //Issuing the query using the helper to the Inventories repository
-            ArrayList<Inventory> list = Helper.query("Inventories", criteria1, context);
-   
             
-            //cerate counter
+            String criteria1 = "/ list";
+            //Issuing the query using the helper to the accounts repository
+            ArrayList<Inventory> inventories = Helper.query("Inventories", criteria1, context);
+            //Create 100 random Items  and add to each Inventory
+            
+            // create a counter
             int i=0;
-
-            for (Inventory inventory : list) {
-
+            for (Inventory inventory : inventories) {
+                
                 String suffix = "";
 
                 if(i>0) {
                     // add security suffix for security items only
                     suffix = " Security";
                 }
-                
-                // create a List to hold the id created for the items
-                ArrayList<Integer> itemIds = new ArrayList();
-                
+
                 // create arrays to hold data to insert into items
                 String[] names = {"Clean View Helix", "Halo UV", "Ball Vac", "Floor Waxer", "Floor Striper"};
                 String[] descriptions = {"Sucks like no other", "can change from suck to blow", "rolls around nicely", "Waxes floors with vigor", "Strips really well"};
@@ -56,61 +52,24 @@ public class ItemCreateTest extends Test {
                 Integer[] sts = {5, 5, 2, 2, 1};
                 
                 
-                for(int j=0; j<names.length; j++) {
+                Item item = new Item(names[i] + suffix);
 
-                    Item item = new Item(names[i] + suffix);
+                item.setName(names[i] + suffix);
+                item.setDescription(descriptions[i]);
+                item.setOem(oems[i]);
+                item.setModelNumber(models[i]);
+                item.setInventoryId(inventory.getId());
+                item.setStockThreshold(sts[i]);
 
+                inventory.insert(item);
 
-                    item.setName(names[i] + suffix);
-                    item.setDescription(descriptions[i]);
-                    item.setOem(oems[i]);
-                    item.setModelNumber(models[i]);
-                    item.setInventoryId(inventory.getId());
-                    item.setStockThreshold(sts[i]);
-
-              
-                    if (!Helper.insert(item, "Inventories", context)) {
-                        System.out.println("insert Item into Item failed!");
-                    }
-                    else {
-                        // insert succeeded, get id and output
-                        String criteria2 = "/list[name='" + item.getName() + "']/getId";
-                        ArrayList<Integer> list2 = Helper.query("Inventories", criteria2, context);
-                        itemIds.add(list2.get(0));
-                        
-                        System.out.println(item.getId());
-                    
-                        // insert output
-                        System.out.println("Inserted :" + item.getTitle());
-                    }
-                    
-                    
-                
-                }
-                
-                inventory.setServices(itemIds);
-
-                //Inventory repository must be updated with the item since
-                //the item is updated with new stock
-                inventory.update(inventory, new WorkDate());
-
-                /*
                 if (!Helper.insert(inventory, "Inventories", context)) {
-                    System.out.println("insert Stock into Items  failed!");
+                    System.out.println("insert Item into Inventory failed!");
                 }
-                */
-
-                // increment the counter
-                i++;
-                
-                // insert output
-                System.out.println("updated :" + inventory.getTitle());
-
-                
-
             }
-
             System.out.print("Insert finished");
+
+
         }
         catch (Exception e) {
             System.out.println(e);
